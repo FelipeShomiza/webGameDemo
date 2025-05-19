cc9d8749d656547f14dac1b473b4af61d6ce3303
FelipeShomiza <felipe.shomiza@gmail.com>
2025-05-19 19:48:35 -0300

    adicionado, camera, players, ondas e plataformas

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import kaboom from "./libs/kaboom.mjs"
+import { Player } from "./entities/Player.js"
+import { Camera } from "./utils/Camera.js"
+import { level1Layout, level1Mappings } from "./content/level1/level1Layout.js"
+import { Level } from "./utils/Level.js"
 import { load } from "./utils/loader.js"
 import { uiManager } from "./utils/UIManager.js"
 
@@ -19,10 +23,29 @@ const scenes = {
         uiManager.displayMainMenu()
     },
     controls: () => {
-
+        uiManager.displayControlsMenu()
     },
     1: () => {
+        setGravity(1400)
 
+        const level1 = new Level()
+        level1.drawBackground("forest-background")
+        level1.drawMapLayout(level1Layout, level1Mappings)
+
+        const player = new Player(
+            1500,
+            100,
+            400,
+            650,
+            3,        
+            1,
+            false 
+        )
+
+        const camera = new Camera()
+        camera.attach(player.gameObj, 0, 200)
+
+        level1.drawWaves("water", "wave")
     },
     2: () => {
 
--- utils/UIManager.js
+++ utils/UIManager.js
@@ -51,7 +51,7 @@ class UIManager {
         ])
 
         this.displayBlinkingUIMessage(
-            "Press [ Enter ] to Start Game",
+            "Aperte [ Enter ] para Comecar o Jogo",
             vec2(center().x, center().y + 100)
         )
 
@@ -59,7 +59,52 @@ class UIManager {
             play("confirm-ui", {speed: 1.5})
             go("controls")
         })
-}
+    }
+    
+    displayControlsMenu() {
+        add([
+            sprite("forest-background"),
+            scale(4)
+            
+        ])
+        add([
+            text("Controles", { font: "Round", size: 50 }),
+            area(),
+            anchor("center"),
+            pos(center().x, center().y - 200),
+        ])
+
+        const controlPrompts = add([
+            pos(center().x + 30, center().y),
+        ])
+        controlPrompts.add([
+            sprite("up"),
+            pos(0, -80),
+        ])
+        controlPrompts.add([sprite("down")])
+        controlPrompts.add([sprite("left"), pos(-80, 0)])
+        controlPrompts.add([sprite("right"), pos(80, 0)])
+        controlPrompts.add([sprite("space"), pos(-200, 0)])
+        
+        controlPrompts.add([
+            text("Pular", { font: "Round", size: 32 }),
+            pos(-198, 100),
+        ])
+
+        controlPrompts.add([
+            text("Mover", { font: "Round", size: 32 }),
+            pos(0, 100),
+        ])
+
+        this.displayBlinkingUIMessage(
+            "Aperte [ Enter ] para Comecar o Jogo",
+            vec2(center().x, center().y + 300)
+        )
+        onKeyPress("enter", () => {
+            play("confirm-ui", { speed: 1.5 })
+            go(1)
+        })
+    }
 }
 
 
